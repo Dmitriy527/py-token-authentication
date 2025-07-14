2d2bbe38673e7687b09e9235558038d91a4e7ae4
Dmytro <dimkanividimka4@gmail.com>
2025-07-14 15:44:16 +0300

    Solution

--- cinema/permissions.py
+++ cinema/permissions.py
@@ -7,11 +7,11 @@ class IsAdminOrIfAuthenticatedReadOnly(BasePermission):
     """
 
     def has_permission(self, request, view):
-        return (
-                bool(request.user and request.user.is_staff)
-                or (
-                        request.method in SAFE_METHODS
-                        and request.user
-                        and request.user.is_authenticated
-                )
+        return bool(
+            (request.user and request.user.is_staff)
+            or (
+                    request.method in SAFE_METHODS
+                    and request.user
+                    and request.user.is_authenticated
+            )
         )
